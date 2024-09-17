d8c5b65428d03eb9116484b454895d960acc3fc7
Julian Ruestig <jruestig@posteo.de>
2024-09-17 12:42:26 +0200

    jwst: deleting old files

--- src/_jwst/__init__.py
+++
@@ -1,7 +0,0 @@
-# from . import integration_model
-# from . import wcs
-# from . import mock_data
-
-# from .config_handler import *
-# from .jwst_data import JwstData
-# from .reconstruction_grid import Grid
